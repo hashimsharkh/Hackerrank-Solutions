d1c47f1c78326e45b3f5336ea15d70d13bb27797
hashimsharkh <hashimabusharkh@gmail.com>
2019-03-16 16:17:32 -0400

    Added Solutions to Data Structures -Linked Lists

--- Data Structures/Linked Lists/InsertNodeAtHead/InsertNodeAtHead.java
+++ Data Structures/Linked Lists/InsertNodeAtHead/InsertNodeAtHead.java
@@ -54,7 +54,13 @@ public class InsertNodeAtHead {
      *
      */
     static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        
+        SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+        node.next = llist;
+        llist = node;
 
+
+        return llist;
 
     }
 
