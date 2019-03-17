fb817cab7906b0a7db75488aa35b40ee6f987244
hashimsharkh <hashimabusharkh@gmail.com>
2019-03-17 16:07:44 -0400

    Update CompareTwoLinkedListsSolution.java

--- Data Structures/Linked Lists/CompareTwoLinkedLists/CompareTwoLinkedListsSolution.java
+++ Data Structures/Linked Lists/CompareTwoLinkedLists/CompareTwoLinkedListsSolution.java
@@ -72,24 +72,38 @@ public class CompareTwoLinkedListsSolution {
     
     return head1.data == head2.data && compareLists(head1.next,head2.next);
  
-    //Iterative method
+    //Iterative method run time of O(n) where n is the number of elements in the linked list with the bigger size
     /*
-        if(head1==null &&head2==null)))
-            return true;
-        
-        while(head1.data==head2.data)
-        {
-            if(head1.next==null &&head2.next==null)
-                return true;
-            else 
-                return false;
+        boolean flag = true;
+        int sizeOfList1=0;
+        int sizeOfList2=0;
+        SinglyLinkedListNode current1=head1;
+        SinglyLinkedListNode current2=head2;
 
-            head1=head1.next;
-            head2 = head2.next;
+        while(head1!=null)
+        {
+            sizeOfList1++;
+            head1 = head1.next;    
+        }
+        while(head2!=null)
+        {
+            sizeOfList2++;
+            head2=head2.next;
         }
 
-        return false;
+        if (sizeOfList1!=sizeOfList2)
+            return false;
         
+        while(current1!=null)
+        {
+            if(current1.data != current2.data)
+                flag = false;
+        current1=current1.next;
+        current2 = current2.next;
+        }
+        return flag;
+
+
     */
 
     }
